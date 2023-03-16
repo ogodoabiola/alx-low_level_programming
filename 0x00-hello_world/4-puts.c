16a6f1c3c1327094a02b1c6582fa0f94f5024876
Abiola Ogodo <whizbee01@gmail.com>
2023-03-16 20:33:49 +0000

    adding puts code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,5 +3,6 @@
 * main - Entry point
 * Description: 'the program's description'
 * @parameter: describe the parameter
-* Return: Always 0 (Success) */
+* Return: Always 0 (Success) 
+*/
 int main(void) { puts("\"Programming is like building a multilingual puzzle"); return 0; }
