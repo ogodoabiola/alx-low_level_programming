e3a51323035078909d7afd70deae6eefe182eac3
Abiola Ogodo <whizbee01@gmail.com>
2023-03-18 21:48:06 +0000

    lower case alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   * Return: always 0 (Success)
+ *    **/
+int main(void)
+{
+int x;
+for (x = 'z'; x >= 'a'; x--)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
