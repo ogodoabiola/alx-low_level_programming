6942465d1c1e3d5664357db67be71977d0a6873f
Abiola Ogodo <whizbee01@gmail.com>
2023-03-18 21:04:23 +0000

    This is listing numbers using the putchar command

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *  *  * main - Entry point
+ *   *   * Return: always 0 (Success)
+ *    *    **/
+int main(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+putchar(48 + n);
+}
+putchar('\n');
+return (0);
+}
