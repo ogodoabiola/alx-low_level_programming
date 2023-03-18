457437a262baf1434e6daa4556022cfd24afc901
Abiola Ogodo <whizbee01@gmail.com>
2023-03-18 16:01:39 +0000

    commit for alphabets without e & q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *  * main - Entry point
+ *   * Return: always 0 (Success)
+ *    **/
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+	if (letter != 'q' && letter != 'e')
+	{
+	putchar(letter);					    }
+	}
+	return (0);
+}
