26de89698b38626d1ef366262675b4025081f9ce
Abiola Ogodo <whizbee01@gmail.com>
2023-03-21 22:39:47 +0000

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include 'main.h'
+
+/**
+ * print_alphabet_x10 - to alphabets 10 times in lower cases
+ */
+
+void print_alphabet_x10(void)
+{
+char c;
+int i = 10;
+while (i--)
+{
+for (c = 'a'; c <= 'z'; ++c)
+putchar(c);
+putchar('\n');
+}
+return (0);
+}
