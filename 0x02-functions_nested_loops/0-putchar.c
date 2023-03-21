d9d9f289789b0596fdd000ad812a73caf850c306
Abiola Ogodo <whizbee01@gmail.com>
2023-03-21 22:02:52 +0000

    task 0 and 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - print	 _putchar
+ * Auth: Abiola Ogodo
+ * This will output the relevant characters
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+int putchar('_');
+int putchar('p');
+int putchar('u');
+int putchar('t');
+int putchar('c');
+int putchar('h');
+int putchar('a');
+int putchar('r');
+int putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - in lower cases
+ * Description: 'this will print in lower cases'
+ */
+
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+_putchar('\n');
+return (0);
+}
