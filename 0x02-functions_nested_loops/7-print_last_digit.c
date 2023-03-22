e525c56ec26415a0f23082bc9216b3e08b609c3c
Abiola Ogodo <whizbee01@gmail.com>
2023-03-22 20:01:31 +0000

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - this is to print the last digit of a number
+ * @n: general argument
+ * Return: 0 Always
+ */
+
+int print_last_digit(int n)
+{
+	n = 12345;
+	n = n % 10;
+	return (n);
+}
