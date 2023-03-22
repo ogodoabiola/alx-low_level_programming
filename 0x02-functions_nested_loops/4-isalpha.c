ead7823e5e8e4b338e06328689a12679066f8d18
Abiola Ogodo <whizbee01@gmail.com>
2023-03-22 17:54:21 +0000

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - for checking c
+ * @c: parameter for arguments
+ * Return: 1 if c is a letter, lowercase or uppercase, else 0
+ */
+
+int _isalpha(int c)
+{
+	if (c != 0 && c <= 'a' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{ return (0);
+}
