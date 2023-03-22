23b3f8de8514b9e7fe5f740af5e7301dbf989401
Abiola Ogodo <whizbee01@gmail.com>
2023-03-22 18:16:02 +0000

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_sign - entry for the print
+ * @n: parameter for the argument
+ * Description: To return signs
+ * Return: 1 or 0 as the case may be
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	_putchar('+');
+	else if (n == 0)
+		return (0);
+	_putchar(0);
+	else if (n < 0)
+		return ("-1");
+	_putchar('-');
+}
