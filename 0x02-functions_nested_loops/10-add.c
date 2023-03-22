7c803c15f91583caa7efbc1eb42ca0a89ec372ab
Abiola Ogodo <whizbee01@gmail.com>
2023-03-22 21:06:01 +0000

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - entry point
+ * @a: for the a params
+ * @b: arguments passed
+ * Return: the addition
+ */
+
+int add(int a, int b)
+{
+	c = (a + b);
+	return (c);
+}
