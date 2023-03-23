c401478f9562399791af897eca4b3a38cb671beb
Abiola Ogodo <whizbee01@gmail.com>
2023-03-23 22:28:33 +0000

    task debug 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * largest_number - returns the largest of 3 numbers
@@ -15,6 +15,10 @@ int largest_number(int a, int b, int c)
 	if (a > b && a > c)
 	{
 	largest = a;
+	}
+	else if (a > b && c > a)
+	{
+	largest = c;
 	}
 	else if (b > c)
 	{
