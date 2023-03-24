299d24acef3e6de3257e605e68204b4bfc4b531b
Abiola Ogodo <whizbee01@gmail.com>
2023-03-24 13:07:11 +0000

    More functions task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - primary fn
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+for (c >= 48; c <= 57; c++)
+char c = c + '0';
+for (c >= 48; c <= 57; c++)
+_putchar(c);
+_putchar('\n');
+return (0);
+}
