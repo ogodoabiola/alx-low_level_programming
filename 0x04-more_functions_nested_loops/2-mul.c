c0922fb11c71b1b616421a6dcda3787d96129bda
Abiola Ogodo <whizbee01@gmail.com>
2023-03-24 12:39:27 +0000

    More functions task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - main entry
+ * @a: parameter for test 1
+ * @b: parameter for test 2
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
+
