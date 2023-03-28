cda5029d79ca090538f897e712c19907c102bce6
Abiola Ogodo <whizbee01@gmail.com>
2023-03-28 19:29:52 +0000

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,28 @@
 #include "main.h"
-#include <string.h>
 
 /**
-* puts2 - main entry
-* @str: for string
-* Return: 0 Always
-*/
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	char tmp;
+	int i, len, len1;
 
-void puts2(char *str)
-{
-int i = 0;
-while (str[i] != '\0')
-{
-_putchar(str[i]);
-i += 2;
-}
-_putchar('\n');
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
+	}
 }
