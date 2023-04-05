6e8de973ca2b979caf08ae0a2c65aa967bf0b2c1
Abiola Ogodo <whizbee01@gmail.com>
2023-04-05 21:25:41 +0000

    return string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - main entry
+ * @s: param
+ * Return: 0
+ */
+
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return (0);
+}
+else
+{
+return (1 + _strlen_recursion(s + 1));
+}
+}
