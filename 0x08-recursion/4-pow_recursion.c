06b1b1921380bb4b7536a05631bd1f9dd1eca72b
Abiola Ogodo <whizbee01@gmail.com>
2023-04-06 16:35:47 +0000

    the power of

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - main entry
+ * @a: param 1
+ * @b: param 2
+ * Return: the power
+ */
+int _pow_recursion(int a, int b)
+{
+	if (a < 0)
+		return (-1);
+	if (b == 0)
+		return (1);
+	return (a * _pow_recursion(a, b - 1));
+}
