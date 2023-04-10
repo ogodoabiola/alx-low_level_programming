a5060a0c792d229cb1797c8bfe598fead6bbc902
Abiola Ogodo <whizbee01@gmail.com>
2023-04-10 11:41:50 +0000

    0. It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * main - the proper entry
@@ -10,10 +9,7 @@
 
 int main(int argc, char *argv[])
 {
-int i;
-for (i = 0; i < argc; i++)
-{
-printf("Argument %d: %s\n", i, argv[i]);
-}
+printf("%s\n", argv[0]);
+(void) argc;
 return (0);
 }
