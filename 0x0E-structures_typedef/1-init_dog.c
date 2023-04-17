73369250e65ec1e36ca07b21817d585b25de1019
Abiola Ogodo <whizbee01@gmail.com>
2023-04-17 22:55:51 +0000

    initialize variable

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - main
+ * @d: long param
+ * struct dog -  main entry
+ * @name: the naming
+ * @age: age param
+ * @owner: the owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
