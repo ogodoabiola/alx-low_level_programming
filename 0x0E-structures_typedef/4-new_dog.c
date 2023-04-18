9f0570d73f5e75fa9b8cd65b1695e7b48c9e8da2
Abiola Ogodo <whizbee01@gmail.com>
2023-04-18 18:54:28 +0000

    4 new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,45 +1,39 @@
-#include <stdlib.h>
 #include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
- * new_dog - creates a new dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
- * Return: pointer to a new dog
+ * new_dog - main entry
+ * @name: the name
+ * @age: what is the age
+ * @owner: the owmer
+ * Return: 0
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	unsigned int nl, ol, i;
-	dog_t *dog;
-
-	if (name == NULL || owner == NULL)
-		return (NULL);
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
-		return (NULL);
-	for (nl = 0; name[nl]; nl++)
-		;
-	nl++;
-	dog->name = malloc(nl * sizeof(char));
-	if (dog->name == NULL)
-	{
-		free(dog);
-		return (NULL);
-	}
-	for (i = 0; i < nl; i++)
-		dog->name[i] = name[i];
-	dog->age = age;
-	for (ol = 0; owner[ol]; ol++)
-		;
-	ol++;
-	dog->owner = malloc(ol * sizeof(char));
-	if (dog->owner == NULL)
-	{
-		free(dog->name);
-		free(dog);
-		return (NULL);
-	}
-	for (i = 0; i < ol; i++)
-		dog->owner[i] = owner[i];
-	return (dog);
+dog_t *new_dog = malloc(sizeof(dog_t));
+if (new_dog == NULL)
+{
+return (NULL);
 }
+
+new_dog->name = malloc(strlen(name) + 1);
+new_dog->owner = malloc(strlen(owner) + 1);
+if (new_dog->name == NULL || new_dog->owner == NULL)
+{
+free(new_dog->name);
+free(new_dog->owner);
+free(new_dog);
+return (NULL);
+}
+
+strcpy(new_dog->name, name);
+strcpy(new_dog->owner, owner);
+
+new_dog->age = age;
+
+return (new_dog);
+}
+
